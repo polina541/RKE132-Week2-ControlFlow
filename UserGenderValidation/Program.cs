4bb50f5e4d7084a4b6d1de439c0d17460edc3004
Polina Smm. <polina.somova@tktk.ee>
2024-02-07 19:04:19 +0200

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Polina!");
+//rakendus küsib kasutajal valida oma sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonanimi]" / "Welcome, Ms. [kasutaja perekonanimi]"
+
+Console.WriteLine("Please, select your gender (m/f)");
+char userGender = Char.Parse(Console.ReadLine()); //loeb maha andmeid string formaadis
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome {userLastName}, uknow gender user!");
+}
+
